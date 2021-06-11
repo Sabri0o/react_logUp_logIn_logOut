0ff0f6801cfe47ae5662c3a1fbdaa5e437eeb8f5
Sabri0o <sabritrabelsi90@gmail.com>
2021-06-11 16:50:28 +0100

    adding nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, Navbar } from "react-bootstrap";
 
 function App() {
-  return <div className="App">Hi there</div>;
+  return (
+    <div className="App">
+      
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">MyApp</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#login">LogIn</Nav.Link>
+            <Nav.Link href="#signup">SignUp</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default App;
