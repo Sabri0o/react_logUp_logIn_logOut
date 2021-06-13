3935bf07ff44d3dd39b3484b93fa73196a92cbbf
Sabri0o <sabritrabelsi90@gmail.com>
2021-06-13 16:30:05 +0100

    login form

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,18 +1,45 @@
 import React from "react";
-import {Form,Button} from "react-bootstrap"
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailOnChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordOnChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-        Login
+      <h3>LogIn form</h3>
       <Form>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            onChange={handleEmailOnChange}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            onChange={handlePasswordOnChange}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
