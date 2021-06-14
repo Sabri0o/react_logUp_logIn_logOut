6396a178cc816c7e0291a1d9e0da72941e6ddf84
Sabri0o <sabritrabelsi90@gmail.com>
2021-06-15 00:17:16 +0100

    nav bar conditional rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,28 +2,38 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Nav, Navbar } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Login from "./components/login";
 import Signup from "./components/signup";
 import Home from "./components/home";
 import Profile from "./components/profile";
 
-
 function App() {
+  const { loginMessage: connectedUserInfo } = useSelector((state) => state);
+  console.log("connectedUserInfo:", connectedUserInfo);
   return (
     <Router>
       <div className="App">
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Navbar.Brand href="/home">MyApp</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="/home">Home</Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link href="/login">LogIn</Nav.Link>
-              <Nav.Link href="/signup">SignUp</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          {connectedUserInfo ? (
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/profile">MyProfile</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          ) : (
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/home">Home</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="/login">LogIn</Nav.Link>
+                <Nav.Link href="/signup">SignUp</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          )}
         </Navbar>
       </div>
 
