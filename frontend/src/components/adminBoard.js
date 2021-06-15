85fa93b5a05ac8d60e98a68d0044caaba5a51427
Sabri0o <sabritrabelsi90@gmail.com>
2021-06-15 18:25:38 +0100

    creating showAllUsers admin service

--- frontend/src/components/adminBoard.js
+++ frontend/src/components/adminBoard.js
@@ -1,28 +1,39 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
-import showAdminContent from "../services/adminService";
+import { useState, useEffect } from "react";
+import { showAdminContent, showAllUsers } from "../services/adminService";
 
 export default function AdminBoard() {
   const [content, setContent] = useState("");
-  const _isMounted = useRef(true);
+  const [allUsers, setAllUsers] = useState([]);
 
   useEffect(() => {
-    if (_isMounted.current) {
-      showAdminContent()
-        .then((response) => {
-          setContent(response);
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-    }
-    return () => {
-      _isMounted.current = false;
-    };
-  }, []);
+    showAdminContent()
+      .then((response) => {
+        setContent(response);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
+  const showUsersInfo = () => {
+    showAllUsers()
+      .then((response) => {
+        setAllUsers(response);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   return (
     <div>
       <h3>{content}</h3>
+      <button onClick={showUsersInfo}>show all users</button>
+      <ul>
+        {allUsers.map((user, index) => {
+          return <li key={index}>{user.email}</li>;
+        })}
+      </ul>
     </div>
   );
 }
--- frontend/src/services/adminService.js
+++ frontend/src/services/adminService.js
@@ -1,14 +1,22 @@
 import $ from "jquery";
-const API_URL = "http://localhost:3001/api/adminBoard";
+const API_URL = "http://localhost:3001/api/";
 
 const userInfo = JSON.parse(localStorage.getItem("connectedUser"));
 
 const showAdminContent = () => {
   return $.ajax({
     method: "GET",
-    url: API_URL,
+    url: API_URL + "adminBoard",
     headers: { "x-access-token": userInfo.accessToken },
   });
 };
 
-export default showAdminContent;
+const showAllUsers = () => {
+  return $.ajax({
+    method: "GET",
+    url: API_URL + "getAllUsers",
+    headers: { "x-access-token": userInfo.accessToken },
+  });
+};
+
+export { showAdminContent, showAllUsers };
