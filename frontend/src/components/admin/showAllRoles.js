6f06f082bd94d8cadeb00af9ca3d2de2d870267f
Sabri0o <sabritrabelsi90@gmail.com>
2021-06-21 18:02:25 +0100

    building a basic ui table with react table

--- frontend/src/components/admin/showAllRoles.js
+++ frontend/src/components/admin/showAllRoles.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { Card } from "react-bootstrap";
+import { useState, useEffect, useMemo } from "react";
+// import { Card } from "react-bootstrap";
 import { getAllRoles } from "../../services/adminService";
+import { useTable } from "react-table";
 
 export default function ShowAllRolesForAdmin() {
   const [allRoles, setAllRoles] = useState([]);
@@ -9,11 +10,39 @@ export default function ShowAllRolesForAdmin() {
   useEffect(() => {
     getAllRoles()
       .then((response) => {
+        console.log(response);
         setAllRoles(response);
       })
       .catch((err) => {
         console.log(err.message);
       });
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Email",
+        accessor: "email",
+      },
+      {
+        Header: "Roles",
+        accessor: "roles",
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => allRoles, [allRoles]);
+
+  const {
+    getTableProps, // Sends the needed props to your table.
+    getTableBodyProps, // Sends needed props to your table body
+    headerGroups, // Returns normalized header groups
+    rows, // rows for the table based on the data passed
+    prepareRow, // Prepare the row in order to be displayed.
+  } = useTable({
+    columns: columns,
+    data: data,
   });
 
   return (
@@ -23,17 +52,37 @@ export default function ShowAllRolesForAdmin() {
       {allRoles.length === 0 ? (
         <div className="container">Loading...</div>
       ) : (
-        <div className="container">
-          {allRoles.map((user, index) => {
-            return (
-              <Card key={index}>
-                <Card.Header>{user.email}</Card.Header>
-                <Card.Body>
-                  <Card.Title>{user.roles.join(' ')}</Card.Title>
-                </Card.Body>
-              </Card>
-            );
-          })}
+        <div className="col-md-8 list">
+          <table
+            className="table table-striped table-bordered"
+            {...getTableProps()}
+          >
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th {...column.getHeaderProps()}>
+                      {column.render("Header")}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row, i) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => {
+                      return (
+                        <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       )}
     </div>
